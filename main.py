db1d66e6dc050608407d85382652cfdfbde6d35d
miked <mikedromey@gmail.com>
2023-05-23 22:55:55 +0100

    functional v1

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import html as html
 import pandas as pd
 from tkinter import filedialog as fd
 import tkinter as tk
 import tkinter.messagebox
-import plotly.graph_objs as go
 import plotly.express as px
 from dash import Dash, html, dcc
+from datetime import datetime
 
 
 def open_file():
@@ -77,7 +76,7 @@ def calc_rolling_mean_hours(df):
     df = df[df['Time to Scan'] <= 86400]
 
     # Resample the data to 2 week intervals and calculate the mean of 'Time to Scan' for each fortnight.
-    fortnight_mean = df.resample('2W', on='Arrival_Date_Time')['Time to Scan'].mean()
+    fortnight_mean = df.resample('7D', on='Arrival_Date_Time')['Time to Scan'].mean()
 
     # Calculate the rolling mean of 'Time to Scan' with a window of 2 weeks.
     rolling_mean = fortnight_mean.rolling(window=1).mean()
@@ -98,6 +97,51 @@ def calc_length_of_ip_stay(df):
     df['Length_of_IP_Stay'] = left_hospital - arrival
 
     return df
+
+
+def add_intervention_markers(fig):
+    intervention_dates = ["03/05/2023", "04/16/2023", "04/28/2023"]
+    intervention_labels = ["Safety brief", "Poster 1", "Poster 2"]
+
+    # Convert date strings to datetime objects
+    intervention_dates = [datetime.strptime(date, "%m/%d/%Y") for date in intervention_dates]
+
+    # Add text labels
+    annotations = []
+    num_labels = len(intervention_labels)
+    y_offset = list(range(1, num_labels + 1))  # Generate offsets based on the number of labels
+    for date, label, offset in zip(intervention_dates, intervention_labels, y_offset):
+        annotation = {
+            'x': date,
+            'y': offset,
+            'text': label,
+            'showarrow': False,
+            'font': {'size': 12, 'color': colors['text']}
+        }
+        annotations.append(annotation)
+
+    # Add marker lines
+    shapes = []
+    for date in intervention_dates:
+        shape = {
+            'type': 'line',
+            'xref': 'x',
+            'yref': 'paper',
+            'x0': date,
+            'y0': 0,
+            'x1': date,
+            'y1': 1,
+            'line': {
+                'color': colors['text'],
+                'width': 1,
+                'dash': 'dash'
+            }
+        }
+        shapes.append(shape)
+
+    fig.update_layout(annotations=annotations, shapes=shapes)
+
+    return fig
 
 
 # read data from Excel file
@@ -122,16 +166,8 @@ fig = px.line(x=rolling_mean_hours.index,
                     "arrival to CT scan? (2 week rolling average)", ).update_layout(
     xaxis_title="Date", yaxis_title="average time to scan (hours)"
 )
+add_intervention_markers(fig)
 
-fig.add_shape(type='line',
-              x0=intervention_date, y0=0, x1=intervention_date, y1=1, yref='paper',
-              line=dict(color='teal', dash='dash'))
-fig.add_annotation(x=intervention_date, y=max(rolling_mean_hours),
-                   text='Intervention 1',
-                   showarrow=True,
-                   arrowhead=1,
-                   arrowcolor=colors['text'],
-                   arrowwidth=2)
 fig.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
@@ -149,15 +185,8 @@ fig2 = px.line(x=df['Arrival_Date_Time'],
     xaxis_title="Date", yaxis_title="Hours"
 )
 
-fig2.add_shape(type='line',
-               x0=intervention_date, y0=0, x1=intervention_date, y1=1, yref='paper',
-               line=dict(color='teal', dash='dash'))
-fig2.add_annotation(x=intervention_date, y=30,
-                    text='Intervention 1',
-                    showarrow=True,
-                    arrowhead=1,
-                    arrowcolor=colors['text'],
-                    arrowwidth=2)
+add_intervention_markers(fig2)
+
 fig2.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
@@ -175,19 +204,12 @@ med_patients_by_week = med_patients.groupby(pd.Grouper(key='Arrival_Date_Time', 
 fig3 = px.bar(med_patients_by_week,
               x='Arrival_Date_Time',
               y='count',
-              title="How many patients with thunderclap headache are admitted to medicine per month?", ).update_layout(
+              title="Patients with thunderclap headache are admitted to medicine per month", ).update_layout(
     xaxis_title="Date", yaxis_title="Number of patients admitted"
 )
 
-fig3.add_shape(type='line',
-               x0=intervention_date, y0=0, x1=intervention_date, y1=1, yref='paper',
-               line=dict(color='teal', dash='dash'))
-fig3.add_annotation(x=intervention_date, y=max(rolling_mean_hours),
-                    text='Intervention 1',
-                    showarrow=True,
-                    arrowhead=1,
-                    arrowcolor=colors['text'],
-                    arrowwidth=2)
+add_intervention_markers(fig3)
+
 fig3.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
@@ -196,6 +218,11 @@ fig3.update_layout(
     xaxis=dict(gridcolor='grey', zerolinecolor='grey')
 )
 
+# Define the initial visible date ranges
+date_range_1 = [datetime(2023, 1, 2), datetime.today()]  # Adjust as needed
+date_range_2 = [datetime(2023, 2, 26), datetime.today()]  # Adjust as needed
+date_range_3 = [datetime(2022, 11, 1), datetime.today()]  # Adjust as needed
+
 app.layout = html.Div(children=[
     html.H1(children='SAH QIP Dashboard'),
 
@@ -203,21 +230,39 @@ app.layout = html.Div(children=[
         SAH Dashboard: Interactive dashboard for the SAH QIP project.
     '''),
 
+    html.Div(
+        children=[
+            html.H3('Intervention Details'),
+            html.P(
+                'Safety brief - We asked the Practice development nurse for the emergency department to remind triage staff to look for patients who could benefit from an early CT head.'),
+            html.P(
+                'Poster 1 - We created a brief poster reminding triage staff to look for patients who need a scan and inform senior doctor. This was placed in the triage rooms. A separate, slightly simpler poster was placed in reception.'),
+            html.P(
+                'Poster 2 - We decided to update the design of the poster to make it more eyecatching and colourful. More posters were placed in several areas of the department.'),
+        ],
+        style={'background-color': colors["background"], 'padding': '20px'}
+    ),
+
     dcc.Graph(
         id='Moving average of time to CT from arrival',
-        figure=fig
+        figure=fig.update_layout(xaxis_range=date_range_1),
+        style={'height': '400px', 'width': '100%'},
+        responsive=True,
     ),
 
     dcc.Graph(
         id='Length of Stay',
-        figure=fig2
+        figure=fig2.update_layout(xaxis_range=date_range_2),
+        style={'height': '400px', 'width': '100%'},
+        responsive=True,
     ),
 
     dcc.Graph(
         id='Number admitted',
-        figure=fig3
+        figure=fig3.update_layout(xaxis_range=date_range_3),
+        style={'height': '400px', 'width': '100%'},
+        responsive=True,
     )
 ])
-
 if __name__ == '__main__':
     app.run_server(debug=True)
